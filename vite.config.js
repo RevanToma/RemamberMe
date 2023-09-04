83abb9ff4cf15cb190701792deb62457dd9a234a
RevanToma <tamrevan@gmail.com>
2023-09-04 12:58:26 +0200

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-RemamberMe/',
+  base: '/RemamberMe/',
   plugins: [vue()],
   resolve: {
     alias: {
